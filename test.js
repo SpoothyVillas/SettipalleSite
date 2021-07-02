7cf9d8b158c86cddeea0a0a2630109fe1bc80eb3
tallapureddys <tallapureddys@gmail.com>
2021-07-02 08:06:00 +0530

    dev branch

---
+++ test.js
@@ -0,0 +1,3 @@
+java is very very powerfull
+sivaddy thopu java l
+I have updated this file
---
+++ test.py
@@ -0,0 +1,4 @@
+ this is python file
+ python is very famous language now
+ so try to learn it
+ all he very best
